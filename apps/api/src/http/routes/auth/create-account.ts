f9d07ceecb7f10e7c161486064db9fa452b1eade
Luís Miguel <luismigueldutraalves@gmail.com>
2025-03-08 12:32:56 -0300

    feat: create auth routes

---
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -0,0 +1,68 @@
+import { hash } from 'bcryptjs'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+import { ResourceAlreadyExistsError } from '../_errors/resource-already-exists-error'
+
+export async function createAccount(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/accounts',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Create a new account',
+        body: z.object({
+          name: z.string().trim().min(1),
+          email: z.string().email(),
+          password: z.string().min(6),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail) {
+        throw new ResourceAlreadyExistsError(
+          'User with same email already exists.',
+        )
+      }
+
+      const [, domain] = email.split('@')
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true,
+        },
+      })
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+          member_on: autoJoinOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinOrganization.id,
+                },
+              }
+            : undefined,
+        },
+      })
+
+      return reply.status(201).send()
+    },
+  )
+}
